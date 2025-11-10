1f0cb60b39f4e0fca00a7d2db7ffb416e85e8f40
idozur1 <idozur7@gmail.com>
2025-11-11 00:19:56 +0200

    added a function that calculates and plots all ffts for aliasing part.

--- Signal_Processing.py
+++ Signal_Processing.py
@@ -2,7 +2,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pn
 from scipy.optimize import curve_fit
-from copy import deepcopy
 from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 
@@ -18,6 +17,7 @@ from scipy.optimize import curve_fit
 #     return all_sheets_dict
 
 def get_data(filename: str) -> dict:
+    ''' this function returns the data as a dict with keys the TAB NAMES and values the data as DF type'''
     all_sheet_dict = pn.read_excel(filename, sheet_name = None)
     for dist, data in all_sheet_dict.items():
         # sheet is a df
@@ -207,6 +207,42 @@ def noise_reduction(data: dict) -> dict:
          new_data[freq] = data[freq] - NOISE_FFT[freq]
      return new_data
 
+# Aliasing:
+
+def alias_plot(filename):
+    alias_data = get_data(filename)
+    for name, df in alias_data.items():
+        time_vals = df.iloc[:,0].tolist()
+        interval = time_vals[1] - time_vals[0]
+        volt_vals = df.iloc[:,1].tolist()
+        meas_spectrum = fourier_trans(volt_vals, interval)[0]
+
+        #plot:
+        x_data = []
+        y_data = []
+        for freq, amp in meas_spectrum.items():
+            if freq >= 0:
+                x_data.append(freq)
+                y_data.append(amp)
+        sorted_pairs = sorted(zip(x_data, y_data))
+        freqs = [pair[0] for pair in sorted_pairs]
+        amps = [pair[1] for pair in sorted_pairs]
+        plt.figure(figsize = (10, 5))
+        plt.plot(freqs, amps)
+        plt.title(str(name))
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Amplitude (V)")
+        plt.grid(True)
+        plt.show()
+
+alias_plot("alias.xlsx")
+
+
+
+
+
+
+# week 3:
 
 def demodulate_AM_wave(filename, carrier_freq) -> dict:
     # works only for cosine carrier waves. doesnt work for sine or a combination.
@@ -242,7 +278,7 @@ def demodulate_AM_wave(filename, carrier_freq) -> dict:
    # am_wave_spectrum = fourier_trans(am_wave.iloc[:,1],interval)
     # shift by w0 to the right
     #move_right_wave = deepcopy(am_wave_spectrum)
-    for freq in move_right_wave[0].keys():
+  #  for freq in move_right_wave[0].keys():
      #   move_right_wave[freq + carrier_freq] = move_right_wave[freq]
       #  move_right_wave[freq] = 0
     # shift by w0 to the right:
