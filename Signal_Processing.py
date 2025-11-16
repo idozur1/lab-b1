b20546713ea80585f56c9e7550f02ad432a2ac00
idozur1 <idozur7@gmail.com>
2025-11-16 21:40:55 +0200

    recovering previous version

--- Signal_Processing.py
+++ Signal_Processing.py
@@ -6,18 +6,9 @@ from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 import scipy.interpolate as interp
 
-# 'frequency2hz.xlsx'
-# 'frequency75hz.xlsx'
-# def get_data(sheet_alias: str) -> dict:
-#     # make a dictionary from all the tabs
-#     # each tab is one wave measurement with varying distance from the LED
-#     all_sheets_dict = pn.read_excel(sheet_alias, sheet_name=None)
-#     for sheet in all_sheets_dict.values():
-#         # cut unnecessary data
-#         sheet = sheet.iloc[7:, :2]
-#     return all_sheets_dict
+# helper functions:
 
-def get_data(filename: str) -> dict:
+def get_data1(filename: str) -> dict:
     ''' this function returns the data as a dict with keys the TAB NAMES and values the data as DF type'''
     all_sheet_dict = pn.read_excel(filename, sheet_name = None)
     for dist, data in all_sheet_dict.items():
@@ -25,43 +16,57 @@ def get_data(filename: str) -> dict:
         all_sheet_dict[dist] = data.iloc[7:, :2]
     return all_sheet_dict
 
-def fourier_trans(data: list, period) -> tuple:
-    #example usage: fourier_trans([1,8,1,0,0,5,4,2 5], 0.5) for a 4 sec measurement in 2 Hz (4*2=8 items in list)
+def get_data2(filename: str) -> dict:
+    ''' this function returns the data as a dict with keys the TAB NAMES and values the data as DF type'''
+    all_sheet_dict = pn.read_excel(filename, sheet_name = None)
+    output = {}
+    for tab, data in all_sheet_dict.items():
+        data = data.iloc[10:,:2]
+        data_dict = {}
+        for i in range(len(data.iloc[:,1])):
+            data_dict[data.iloc[i,0]] = data.iloc[i,1]
+        output[tab] = data_dict
+    return output
 
-    length = len(data)
-    fft = np.abs(np.fft.fft(data,
-                            norm='forward'))  # norm = 'forward' divides rsults by n number of counts keeping normalization (inverse fourier requires no further division)
-    output_freqs = np.fft.fftfreq(length, period)
-    output_data = {}
-    for i in range(length):
-        output_data[output_freqs[i]] = fft[i]
-
-    return output_data, period
-
-BG_NOISE = pn.read_excel("noise.xlsx")
-fft_noise_dict = fourier_trans(BG_NOISE.iloc[:,1].tolist(), 0.0002)[0]
-noise_freqs = sorted([f for f in fft_noise_dict.keys() if f>=0])
-noise_amps = [fft_noise_dict[f] for f in noise_freqs]
-NOISE_INTERP = interp.interp1d(noise_freqs, noise_amps, bounds_error=False, fill_value=0)
+def get_time_interval(data:dict) -> float:
+    ''' this function gets a measurment dict with keys (meas times) in seconds and returns delta t'''
+    meas_times = np.array(list(data.keys()))
+    time_interval = np.abs(np.average([meas_times[i+1]-meas_times[i] for i in range(len(meas_times)-1)]))
+    return time_interval
 
 
-def noise_reduction(data: dict) -> dict:
-    # recieves a dict of freq:volt values in frequency doamin
-    new_data = {}
-    for freq, amp in data.items():
-        if freq >= 0:
-            estimated_noise = NOISE_INTERP(freq)
-            new_data[freq] = max(0, amp - estimated_noise)
-        else:
-            new_data[freq] = amp
-    return new_data
+def get_interest_freqs(fft: dict, min_freq, max_freq, with_0 = True) -> dict:
+    interest_output = {}
+    for freq, value in fft.items():
+        if (freq < max_freq) and (freq > min_freq) and not((with_0 == False) & (freq == 0)):
+            interest_output[freq] = value
+    return interest_output
 
-#define fit
+def get_residuals(data:dict, fit_func):
+
+    residuals = {}
+
+    for w, value in data.items():
+        residuals[w] = value-fit_func(w)
+
+    return residuals
+
+# fitting functions:
+
+def sinc(w:float,A:float,T:float):
+    return A*T*np.sinc(w*T) #check Matap 2 Week 7 Page 12 for pituach
+
+def sinc2(w:float,A:float,T:float):
+    return sinc(w,A,T)**2
+
+def tri_fourier_fit(x):
+    # need to complete this
+    ...
 
 def fit_func(r, k, b):
         return k/r**2 + b
 
-def get_params(x_data, y_data):
+def get_fit_params(fit_func, x_data, y_data):
 
     params, covariance = curve_fit(fit_func, x_data, y_data)
     output = params, covariance
@@ -69,42 +74,151 @@ def get_params(x_data, y_data):
     return output
 
 def fit(x_data, y_data):
-    params = get_params(x_data, y_data)[0]
+    params = get_fit_params(x_data, y_data)[0]
     y_fit = [fit_func(i,params[0],params[1]) for i in x_data]
 
     return y_fit
 
-#Analysis
+def min_max_fit_model(r, k, c):
+    # model of decaying light when light is transmitted as a point source is proportional to 1/r^2.
+    # constant is added for noise in the system'
+    return k/(r**2) + c
 
-def analyse_fourier(dataset: dict) -> dict:
+def min_max_fit_func(data_dict: dict, fit_model):
+    x_values = np.array([key for key in data_dict.keys()])
+    y_values = np.array([val for val in data_dict.values()])
+    popt, pcov = curve_fit(fit_model, x_values, y_values)
+    x_fit = np.linspace(min(x_values), max(x_values), 100)
+    y_fit = fit_model(x_fit, *popt)
+    return (x_fit, y_fit, popt)
 
-    results = {}
+# Analysis functions:
+
+def fourier_trans(data: dict, with_0:bool, time_interval={'mode':'auto', 'value': None}, min_freq = -500, max_freq = 500) -> dict:
+    #example usage: fourier_trans([1,8,1,0,0,5,4,2 5], 0.5) for a 4 sec measurement in 2 Hz (4*2=8 items in list)
+
+    #setting variables
+
+    meas_times = np.array(list(data.keys()))
+    meas_values = np.array(list(data.values()))
+
+    if time_interval['mode'] == 'auto':
+        time_interval = get_time_interval(data)
+    else:
+        time_interval = time_interval['value']
 
     #calculate fft and save to dict
 
-    for dist, data in dataset.items():
-        volt_data = list(data.iloc[:,1])
-        length = len(volt_data)
-        fft = np.abs(np.fft.fft(volt_data, norm = 'forward')) #normalization problem - amp too high in close ditsances
-        output_freqs = np.fft.fftfreq(length, 0.001) #since measurement is in 1/ms
-        output = {}
-        for i in range(len(output_freqs)):
-            output[output_freqs[i]] = fft[i]
-        output = noise_reduction(output)
+    fft = (np.fft.rfft(meas_values,
+                            norm='forward'))  # norm = 'forward' divides results by n number of counts keeping normalization (inverse fourier requires no further division)
+    output_freqs = np.fft.rfftfreq(len(meas_values), time_interval) #size is floor(len(fft)/2) because in rfft only positive values are calculated
+    output_data = {}
+    for i in range(len(output_freqs)):
+        output_data[output_freqs[i]] = fft[i]
+
+    output_data = get_interest_freqs(output_data, min_freq, max_freq, with_0)
+
+    output = {'fourier_trans': output_data,
+              'time_interval': time_interval}
+
+    return output
+
+def file_fourier_trans(dataset: str, with_0:bool, time_interval={'mode':'auto', 'value': None}, min_freq = -500, max_freq = 500) -> dict:
+    ''' this function returns a dict with tab_name:df(fft:amp)'''
+    output = {}
+
+    dataset = get_data2(dataset)
+
+    for tab, data in dataset.items():
+        output[tab] = fourier_trans(data, with_0, time_interval, min_freq, max_freq)
+
+    return output
+
+def dataset_fourier_trans(dataset: dict, with_0: True, min_freq: float, max_freq: float, time_interval={'mode':'auto', 'value': None}) -> dict:
+
+    results = {}
+
+    for tab, data in dataset.items():
+
+        output = fourier_trans(data, with_0, time_interval)['fourier_trans']
+
+        #save results
+
+        max_amp_freq = max(output, key=lambda k: np.abs(output[k]))
+        max_amp = output[max_amp_freq]  # Get the complex value
+        results[max_amp_freq] = max_amp
+
+    return results
+
+def analyze_min_max(data :dict, freq: int):
+    result_dict ={}
+    for dist in data:
+        measurement = data[dist].iloc[:, 1].values
+        # 10% error range for peak find:
+        min_dist = int((len(measurement)/freq)*0.9)
+        peaks = list(measurement[i] for i in find_peaks(measurement, distance=min_dist)[0])
+        valleys = list(measurement[j] for j in find_peaks(-measurement, distance=min_dist)[0])
+        amp = (np.mean(peaks) - np.mean(valleys)) / 2
+        result_dict[int(dist[:2])] = amp
+    return result_dict
+
+# noise reduction:
+
+BG_NOISE_df = pn.read_excel("noise.xlsx")
+BG_NOISE_df = BG_NOISE_df.iloc[10:, :2]
+noise_data_dict = {}
+for i in range(len(BG_NOISE_df.iloc[:,1])):
+    noise_data_dict[BG_NOISE_df.iloc[i,0]] = BG_NOISE_df.iloc[i,1]
+fft_noise_output = fourier_trans(noise_data_dict, with_0=True, time_interval={'mode':'manual', 'value': 0.0002})
+fft_noise_dict = fft_noise_output['fourier_trans']
+noise_freqs = sorted([f for f in fft_noise_dict.keys() if f>0])
+noise_amps = [np.abs(fft_noise_dict[f]) for f in noise_freqs]
+noise_amps_db = []
+NOISE_INTERP = interp.interp1d(noise_freqs, noise_amps, bounds_error=False, fill_value=0)
+
+def noise_reduction(data: dict) -> dict:
+    # recieves a dict of freq:volt values in frequency doamin
+    new_data = {}
+    for freq, amp in data.items():
+        if freq >= 0:
+            estimated_noise = NOISE_INTERP(freq)
+            amp_mag = np.abs(amp)
+            new_data[freq] = max(0, amp_mag - estimated_noise)
+        else:
+            new_data[freq] = amp
+    return new_data
+
+# old function:
+# old analyse_fourier func
+# def analyse_fourier(dataset: dict) -> dict:
+
+  #  results = {}
+
+    #calculate fft and save to dict
+
+    #for dist, data in dataset.items():
+     #   volt_data = list(data.iloc[:,1])
+      #  length = len(volt_data)
+       # fft = np.abs(np.fft.fft(volt_data, norm = 'forward')) #normalization problem - amp too high in close ditsances
+        #output_freqs = np.fft.fftfreq(length, 0.001) #since measurement is in 1/ms
+        #output = {}
+        #for i in range(len(output_freqs)):
+        #    output[output_freqs[i]] = fft[i]
+        #output = noise_reduction(output)
         #save result
 
-        amp = max(fft)
-        results[dist] = amp
+       # amp = max(fft)
+       # results[dist] = amp
 
         #trim dict to interest frequencies
 
-        interest_output = {}
-        for freq,value in output.items():
-            if freq < 100 and freq > 0:
-                interest_output[freq] = value
+       # interest_output = {}
+       # for freq,value in output.items():
+        #    if freq < 100 and freq > 0:
+         #       interest_output[freq] = value
 
-        max_value = max(interest_output.values())
-        max_amp_freq, max_amp = [(key, value) for key, value in interest_output.items() if value == max_value][0]
+        #max_value = max(interest_output.values())
+        #max_amp_freq, max_amp = [(key, value) for key, value in interest_output.items() if value == max_value][0]
 
         #plot fourier trasnform
         #
@@ -121,49 +235,31 @@ def analyse_fourier(dataset: dict) -> dict:
         # #plt.legend()
         # plt.show()
 
-    return(results)
+    #return(results)
 
-def analyze_min_max(data :dict, freq: int):
-    result_dict ={}
-    for dist in data:
-        measurement = data[dist].iloc[:, 1].values
-        # 10% error range for peak find:
-        min_dist = int((len(measurement)/freq)*0.9)
-        peaks = list(measurement[i] for i in find_peaks(measurement, distance=min_dist)[0])
-        valleys = list(measurement[j] for j in find_peaks(-measurement, distance=min_dist)[0])
-        amp = (np.mean(peaks) - np.mean(valleys)) / 2
-        result_dict[int(dist[:2])] = amp
-    return result_dict
 
-def min_max_fit_model(r, k, c):
-    # model of decaying light when light is transmitted as a point source is proportional to 1/r^2.
-    # constant is added for noise in the system'
-    return k/(r**2) + c
 
-def min_max_fit_func(data_dict: dict, fit_model):
-    x_values = np.array([key for key in data_dict.keys()])
-    y_values = np.array([val for val in data_dict.values()])
-    popt, pcov = curve_fit(fit_model, x_values, y_values)
-    x_fit = np.linspace(min(x_values), max(x_values), 100)
-    y_fit = fit_model(x_fit, *popt)
-    return (x_fit, y_fit, popt)
+
 
 #results plot
 
-def get_fourier_plot(files,x_mistake):
+# plot functions:
+
+def get_fourier_plot(files,x_mistake, max_freq, min_freq):
+    #plots the final plot of week 1, based on multiple Fourier analysises
     plt.title("Amplitude VS Distance - Fourier method")
     atten_results = {}
     for freq, filepath in files.items():
-        dataset = get_data(filepath)
-        results = analyse_fourier(get_data(filepath))
+        dataset = get_data2(filepath)
+        results = fourier_trans(get_data(filepath),max_freq, min_freq)
         for key, value in results.items():
             atten_results[int(key[0:len(files.keys())])] = value-x_mistake
 
         x_data = list(atten_results.keys())
         y_data = list(atten_results.values())
 
-        x_for_fit = [i for i in range(10,100)]
-        params = get_params(x_data,y_data)[0]
+        x_for_fit = [i for i in range(min_freq,max_freq)]
+        params = get_fit_params(x_data,y_data)[0]
         y_for_fit = [fit_func(i, params[0],params[1]) for i in x_for_fit]
 
         plt.scatter(x_data, y_data, label = f"{freq} Hz Data")
@@ -187,55 +283,103 @@ def get_min_max_plot(amp_dist_dict: dict, name: str):
     plt.ylabel("Amplitude (V)")
     plt.title("Amplitude VS Distance - min-max method")
 
-files = {"2": "frequency2hz.xlsx",
-            "75": "frequency75hz.xlsx"}
-x_mistake = 0.0097
+def plot_fourier_transform(data:dict, title:str, xlabel:str, ylabel:str, fit_func = None, residuals_flag = False):
 
-get_fourier_plot(files,x_mistake)
-get_min_max_plot(analyze_min_max(get_data(files["2"]), 2), "2Hz")
-get_min_max_plot(analyze_min_max(get_data(files["75"]), 75), "75Hz")
-plt.show()
+    #variabales
 
-#fit test
-#
-# x_data = [1,2,3,4]
-# y_data = [1,1/4,1/9,1/16]
-#
-# params = get_params(x_data,y_data)[0]
-# y_fit = fit(x_data,y_data)
-#
-# plt.scatter(x_data,y_fit, label = 'fit')
-# plt.scatter(x_data, y_data, label = 'org')
-# plt.legend()
-# plt.show()
-#title = 'Ampltiude (V) as a function of distance (m)'
+    freqs = [float(i) for i in data.keys()]
+    values = [np.abs(i) for i in data.values()]
+
+    #get fit
+    if fit_func is not None:
+        params = get_fit_params(fit_func,freqs,values)[0]
+
+        def fit_func_with_params(w):
+            return fit_func(w,params[0],params[1])
+
+        fit_data = {}
+        for w in freqs:
+            fit_data[w] = fit_func_with_params(w) #assumes fit function with 2 fitting parameters
+        plt.plot(fit_data.keys(),fit_data.values(), label = 'fit', color = 'orange')
+
+    #get residuals
+    if residuals_flag == True:
+        residuals = get_residuals(data, fit_func_with_params)
+        plt.scatter(residuals.keys(), residuals.values(), label = 'residuals')
+
+    #plot
+
+    plt.title(title)
+    plt.plot(freqs, values, label='fft')
+    # plt.annotate(f'Freq: {max_amp_freq:.2f}, Amplitude: {max_amp:.2f}',
+    #              xy = (max_amp_freq, max_amp),
+    #              xytext = (max_amp_freq, max_amp),
+    #              arrowprops=dict(facecolor='black', shrink=0.05),  # Arrow properties
+    #              fontsize=10,
+    #              color='red')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.show()
+
+# Main Functions:
+
+def get_rect_transform(fit_func, residuals_flag = False):
+    dataset = file_fourier_trans('triangle.xlsx', min_freq=0, max_freq=100, with_0=True)
+    for tab, output in dataset.items():
+        peaks = {}
+
+        for key, value in output['fourier_trans'].items():
+            if np.abs(value) > 0.002:
+                peaks[key] = value
+
+        plot_fourier_transform(peaks, tab, residuals_flag=residuals_flag, xlabel='Frequency (Hz)', ylabel='Amplitude (Volts)', fit_func=fit_func)
+
+get_rect_transform(sinc2, residuals_flag = False)
+
+def week1_main():
+    files = {"2": "frequency2hz.xlsx",
+             "75": "frequency75hz.xlsx"}
+    x_mistake = 0.0097
+
+    get_fourier_plot(files, x_mistake)
+    get_min_max_plot(analyze_min_max(get_data1(files["2"]), 2), "2Hz")
+    get_min_max_plot(analyze_min_max(get_data1(files["75"]), 75), "75Hz")
+    plt.show()
 
 # week 2:
 
-# move fourier trans and noise reduction to the beginning of the code
+# plot noise graph, change to db in the beggining of the code if needed
 
-
+#plt.plot(noise_freqs, noise_amps)
+#plt.xlim(0, 500)
+#plt.xlabel("Frequency (Hz)")
+#plt.ylabel("Amplitude (dB)")
+#plt.grid(True)
+#plt.show()
 
 # Aliasing:
 
 def alias_plot(filename):
-    alias_data = get_data(filename)
+    alias_data = get_data1(filename)
     for name, df in alias_data.items():
         time_vals = df.iloc[:,0].tolist()
         interval = time_vals[1] - time_vals[0]
         volt_vals = df.iloc[:,1].tolist()
-        meas_spectrum = fourier_trans(volt_vals, interval)[0]
+        data_dict = dict(zip(time_vals, volt_vals))
+        fft_output = fourier_trans(data_dict, with_0=True, time_interval = {'mode':'manual', 'value':interval})
+        meas_spectrum = fft_output['fourier_trans']
         filtered_spectrum = noise_reduction(meas_spectrum)
         #plot:
         x_data = []
         y_data = []
-        for freq, amp in meas_spectrum.items(): #change meas_spectrum to filtered_spectrum to get noise reduction
+        for freq, amp in filtered_spectrum.items(): #change meas_spectrum to filtered_spectrum to get noise reduction
             if freq >= 0:
                 x_data.append(freq)
                 y_data.append(amp)
-        #epsilon = 1e-12 # small epsilon o vaoid log(0)
-      #  for i in range(len(y_data)): # convert V -> dB
-            #y_data[i] = 20 * np.log10(y_data[i] + epsilon)
+        epsilon = 1e-12 # small epsilon to avoid log(0)
+        #for i in range(len(y_data)): # convert V -> dB
+          #  y_data[i] = 20 * np.log10(np.abs(y_data[i]) + epsilon)
         sorted_pairs = sorted(zip(x_data, y_data))
         freqs = [pair[0] for pair in sorted_pairs]
         amps = [pair[1] for pair in sorted_pairs]
@@ -251,7 +395,7 @@ def alias_plot(filename):
 alias_plot("alias.xlsx")
 
 
-plt.plot(noise_freqs, noise_amps)
+
 
 
 
@@ -260,7 +404,7 @@ plt.plot(noise_freqs, noise_amps)
 def demodulate_AM_wave(filename, carrier_freq) -> dict:
     # works only for cosine carrier waves. doesnt work for sine or a combination.
     # take the df of the wave in time space
-    am_wave = get_data(filename).values()
+    am_wave = get_data1(filename).values()
     t = np.array(am_wave[:, 0])
     # make local carrier cos(w0t)
     loc_carrier = np.cos(2*np.pi*carrier_freq*t)
@@ -268,7 +412,7 @@ def demodulate_AM_wave(filename, carrier_freq) -> dict:
     mod_wave = am_wave[:, 1] * loc_carrier
     # analyze fourier:
     interval = am_wave.iloc[2,0]-am_wave.iloc[1,0]
-    mod_wave_spectrum = fourier_trans(mod_wave,interval)
+   # mod_wave_spectrum = fourier_trans(mod _wave,interval)
 
     # READ THIS!!
     # need to finish -
